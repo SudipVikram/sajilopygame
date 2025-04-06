6b4f7cae0c13d6fa1cdfe2b51c6dabf9c20b3e96
SudipVikram <sudip.beyondapogee@gmail.com>
2025-04-06 08:48:33 +0545

    mapping of keystrokes for player and enemy done

--- sajilopygame.py
+++ sajilopygame.py
@@ -9,16 +9,80 @@ class sajilopygame:
         pygame.init()
         self.screen = pygame.display.set_mode((self.swidth,self.sheight))
 
+        # Movement states for keys
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+
+        # Mappings
+        self.is_lr_mapped_to_player = False
+        self.is_ud_mapped_to_player = False
+        self.is_lr_mapped_to_enemy = False
+        self.is_ud_mapped_to_enemy = False
+
     # function to update the display window
     # also is responsible for quitting the program
     def refresh_window(self):
         # updating the window
         pygame.display.update()
 
-        # checking for a close window event
+        # Checking for window events
         for event in pygame.event.get():
+            # If close button is pressed
             if event.type == pygame.QUIT:
                 pygame.quit()
+                exit()
+            # upon key presses
+            if event.type == pygame.KEYDOWN:
+                # If Esc button is pressed
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    exit()
+                # Movement key pressed
+                if event.key == pygame.K_LEFT:
+                    self.left_pressed = True
+                if event.key == pygame.K_RIGHT:
+                    self.right_pressed = True
+                if event.key == pygame.K_UP:
+                    self.up_pressed = True
+                if event.key == pygame.K_DOWN:
+                    self.down_pressed = True
+
+            # Upon key release
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    self.left_pressed = False
+                if event.key == pygame.K_RIGHT:
+                    self.right_pressed = False
+                if event.key == pygame.K_UP:
+                    self.up_pressed = False
+                if event.key == pygame.K_DOWN:
+                    self.down_pressed = False
+
+        # Update player position based on the key press state
+        if self.is_lr_mapped_to_player:
+            if self.left_pressed:
+                self.playerx -= self.player_lr_intensity
+            if self.right_pressed:
+                self.playerx += self.player_lr_intensity
+        if self.is_ud_mapped_to_player:
+            if self.up_pressed:
+                self.playery -= self.player_ud_intensity
+            if self.down_pressed:
+                self.playery += self.player_ud_intensity
+
+        # Update enemy position based on the key press state
+        if self.is_lr_mapped_to_enemy:
+            if self.left_pressed:
+                self.enemyx -= self.enemy_lr_intensity
+            if self.right_pressed:
+                self.enemyx += self.enemy_lr_intensity
+        if self.is_ud_mapped_to_enemy:
+            if self.up_pressed:
+                self.enemyy -= self.enemy_ud_intensity
+            if self.down_pressed:
+                self.enemyy += self.enemy_ud_intensity
 
     # loading the window title
     def window_title(self,title):
@@ -36,10 +100,55 @@ class sajilopygame:
     # loading the background
     def background_image(self,image_path):
         self.background = pygame.image.load(image_path)
+        self.bg_width, self.bg_height = self.background.get_size()
         self.screen.blit(self.background,(0,0))
 
-    # loading a player
-    def player(self,image_path,org=(370,480)):
+    # creating a player
+    def create_player(self,image_path,org=(370,480)):
         self.playerx, self.playery = org
-        self.player = pygame.image.load(image_path)
-        self.screen.blit(self.player,(self.playerx,self.playery))
+        self.player_img = pygame.image.load(image_path)
+        self.player_width, self.player_height = self.player_img.get_size()
+
+    # loading a player
+    def load_player(self):
+        self.screen.blit(self.player_img,(self.playerx,self.playery))
+
+    # creating an enemy
+    def create_enemy(self,image_path,org=(370,40)):
+        self.enemyx, self.enemyy = org
+        self.enemy_img = pygame.image.load(image_path)
+        self.enemy_width, self.enemy_height = self.enemy_img.get_size()
+
+    # loading an enemy
+    def load_enemy(self):
+        self.screen.blit(self.enemy_img,(self.enemyx,self.enemyy))
+
+    # creating an object
+    def create_object(self,image_path,org=(370,240)):
+        self.objectx, self.objecty = org
+        self.object_img = pygame.image.load(image_path)
+        self.object_width, self.object_height = self.object_img.get_size()
+
+    # loading an object
+    def load_object(self):
+        self.screen.blit(self.object_img,(self.objectx,self.objecty))
+
+    # mapping Left, Right keystrokes to player
+    def map_lr_keys_to_player(self,intensity=1):
+        self.is_lr_mapped_to_player = True
+        self.player_lr_intensity = intensity
+
+    # mapping Up, Down keystrokes to player
+    def map_ud_keys_to_player(self,intensity=1):
+        self.is_ud_mapped_to_player = True
+        self.player_ud_intensity = intensity
+
+    # mapping Left, Right keystrokes to enemy
+    def map_lr_keys_to_enemy(self,intensity=1):
+        self.is_lr_mapped_to_enemy = True
+        self.enemy_lr_intensity = intensity
+
+    # mapping Up, Down keystrokes to enemy
+    def map_ud_keys_to_enemy(self,intensity=1):
+        self.is_ud_mapped_to_enemy = True
+        self.enemy_ud_intensity = intensity
--- testing.py
+++ testing.py
@@ -4,11 +4,28 @@ game = sajilopygame()
 game.window_title("Space Invaders")
 game.favicon("ufo.png")
 
+# creating an object bullet
+game.create_object(image_path="bullet.png", org=(370 + 16, 480 + 10))
+# creating a player
+game.create_player(image_path="spaceship.png", org=(370, 480))
+# creating an enemy
+game.create_enemy(image_path="alien.png", org=(370, 40))
+
 while True:
     game.background_color((255, 255, 255))
     game.background_image('background.png')
+    game.load_object()
+    game.load_player()
+    game.load_enemy()
 
-    # creating a player
-    game.player(image_path="spaceship.png",org=(370,480))
+    # map left and right keystrokes to a player
+    game.map_lr_keys_to_player(intensity=5)
+    # map up and down keystrokes to a player
+    game.map_ud_keys_to_player(intensity=5)
+
+    # map left and right keystrokes to an enemy
+    game.map_lr_keys_to_enemy(intensity=5)
+    # map up and down keystrokes to an enemy
+    game.map_ud_keys_to_enemy(intensity=5)
 
     game.refresh_window()
