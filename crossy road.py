7954028f712baa547e63964b5c745a86f60b4dbf
Sudip Vikram Adhikari <32861861+SudipVikram@users.noreply.github.com>
2025-04-14 20:07:16 +0545

    early canvas of crossy road

--- crossy road.py
+++ crossy road.py
@@ -1,6 +1,7 @@
 '''
 @author: Sudip Vikram Adhikari
 @company: Beyond Apogee
+NOTE: This is a game canvas for a more decorated crossy road game.
 A chicken tries to cross a busy road. The road is multiple lanes and there are lots
 of vehicles. You are the chicken and your mission is to successfully navigate around
 the vehicles and cross the road, unless you want to end up becoming a road kill.
@@ -16,21 +17,44 @@ game.window_title("Crossy Road")
 
 # creating characters
 chicken = game.character(parent=game,type="player",player_shape="rectangle",color=(0,255,0),org=(10,10),width=30,height=30,border_thickness=0,border_radius=0)
-vehicle = game.character(parent=game,type="object",player_shape="rectangle",color=(255,0,100),org=(0,100),width=80,height=30,border_thickness=0,border_radius=0)
 
 # multiple vehicles
 vehicles = []
-vehicles.append(vehicle)
+
+# let's have 5 vehicles moving at any given time
+for i in range(5):
+    vehicle = game.character(parent=game,type="object",player_shape="rectangle",
+                             color=game.random_color(),
+                             org=(0, game.random_number(100, game.wheight - 100)),
+                             width=game.random_number(50,100),
+                             height=game.random_number(10,50),border_thickness=0,
+                             border_radius=game.random_number(0,5))
+    vehicles.append(vehicle)
 
 while True:
     game.background_color(color=(0,0,0))
+    # drawing the edges of road # green patches
+    game.draw_rect(color=(0,255,0),xpos=0,ypos=0,width=game.wwidth,height=50,border_thickness=0)
+    game.draw_rect(color=(0,255,0),xpos=0,ypos=game.wheight-50,width=game.wwidth,height=50,border_thickness=0)
     # loading characters
     chicken.load()
 
-    # let's have 5 vehicles moving at any given time
-    for i in range(5):
-        vehicle.load()
-        vehicle.move_right(5)
+    # loading the vehicles
+    for i, vehicle in enumerate(vehicles):
+        if vehicle.alive:
+            vehicle.load()
+            vehicle.move_right(game.random_number(1,5))
+
+            if vehicle.xpos > game.wwidth:
+                vehicle.kill()
+                vehicle = game.character(parent=game, type="object", player_shape="rectangle",
+                                         color=game.random_color(),
+                                         org=(0, game.random_number(100, game.wheight-100)),
+                                         width=game.random_number(50, 100),
+                                         height=game.random_number(10, 50), border_thickness=0,
+                                         border_radius=game.random_number(0, 5))
+                vehicle.update_speed(speed=game.random_number(1, 5))
+                vehicles[i] = vehicle
 
     # setting frames per second
     game.set_fps(60)
